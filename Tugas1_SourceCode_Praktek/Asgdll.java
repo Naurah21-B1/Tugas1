508b3f3f768eaa5960a0d077d028a0d6084a3e2b
Naurah21-B1 <127511514+Naurah21-B1@users.noreply.github.com>
2023-03-11 03:50:46 -0800

    Create Asgdll.java

---
+++ Tugas1_SourceCode_Praktek/Asgdll.java
@@ -0,0 +1,16 @@
+/*Nim : 13020210043
+Nama : Naurah Athayah AR
+Hari/Tanggal : Jumat/10 Maret 2023
+Waktu : 19.18*/
+
+public class Asgdll {
+
+public static void main(String[] args) {
+
+float f= 20.0f;
+double fll;
+
+fll=10.0f;
+System.out.println ("f : "+f+ "\nf11: "+fll);
+}
+}
